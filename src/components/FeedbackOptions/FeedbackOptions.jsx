27656031253991e6401403f508607525423b6568
AlexanderTobert <tobert.sasha@gmail.com>
2023-12-17 16:47:23 +0200

    Update FeedbackOptions.jsx

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,12 +1,14 @@
 import css from './FeedbackOptions.module.css';
+import { nanoid } from 'nanoid'
 
 const FeedbackOptions = ({ options, handleFeedback }) => {
     return (
         <>
             <ul className={css.list}>
                 {options.map(btnName => {
+                    const id = nanoid();
                     return (
-                        <li>
+                        <li key={id}>
                         <button
                                 type="button"
                                 onClick={() => handleFeedback(btnName.toLowerCase())}
