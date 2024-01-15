adf43b79a490f2dbad8f3017a27b64de7963408b
AlexanderTobert <tobert.sasha@gmail.com>
2024-01-15 20:55:12 +0200

    refactored to hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,56 @@
+import React, { useState } from 'react';
 import { Statistics } from 'components/Statistics/Statistics';
 import { Section } from 'components/Section/Section';
-import { FeedbackOptions} from 'components/FeedbackOptions/FeedbackOptions'
-import { Component } from 'react';
+import { FeedbackOptions } from 'components/FeedbackOptions/FeedbackOptions';
 import { Notification } from 'components/Notification/Notification';
 
-export class App extends Component {
-  state = {
-      good: 0,
-      neutral: 0,
-      bad: 0
-  }
-  
-  btnElements = Object.keys(this.state);
+const App = () => {
+  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
 
-  handleFeedback = (type) => {
-      this.setState((prevState) => ({
-        ...prevState,
-        [type]: prevState[type] + 1,
-      }));
+  const btnElements = Object.keys(feedback);
+
+  const handleFeedback = (type) => {
+    setFeedback((prevFeedback) => ({
+      ...prevFeedback,
+      [type]: prevFeedback[type] + 1,
+    }));
   };
-    
-  countTotalFeedback = values => values.reduce((a, b) => (a += b));
 
-  countPositiveFeedbackPercentage = (good, totalFeedback) =>
+  const countTotalFeedback = (values) => values.reduce((a, b) => (a += b));
+
+  const countPositiveFeedbackPercentage = (good, totalFeedback) =>
     totalFeedback && Math.round((good / totalFeedback) * 100);
-  
-  render() {
-    const { good, neutral, bad } = this.state;
-      const total = this.countTotalFeedback([good, neutral, bad]);
-      const positivePercentage = this.countPositiveFeedbackPercentage(
-        good,
-        total
-      );
 
+  const total = countTotalFeedback(Object.values(feedback));
+  const positivePercentage = countPositiveFeedbackPercentage(
+    feedback.good,
+    total
+  );
 
-    return (
-      <>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={this.btnElements}
-            handleFeedback={this.handleFeedback}
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={btnElements}
+          handleFeedback={handleFeedback}
+        />
+      </Section>
+
+      <Section title="Statistics">
+        {!total ? (
+          <Notification message="There is no feedback" />
+        ) : (
+          <Statistics
+            good={feedback.good}
+            neutral={feedback.neutral}
+            bad={feedback.bad}
+            total={total}
+            positivePercentage={positivePercentage}
           />
-        </Section>
-
-        <Section title="Statistics">
-          {!total ? (
-            <Notification message="There is no feedback"></Notification>
-          ) : (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positivePercentage={positivePercentage}
-            />
-          )}
-        </Section>
-
-      </>
-      
-    );
-  }
-  
+        )}
+      </Section>
+    </>
+  );
 };
+
+export default App;
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,26 +1,27 @@
-import React from 'react'
+import React from 'react';
 import css from './Statistics.module.css';
 
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
-    const textElements = ['Good', 'Neutral', 'Bad', 'Total', 'Positive feedback'];
-    const values = [good, neutral, bad, total, positivePercentage];
+  const textElements = ['Good', 'Neutral', 'Bad', 'Total', 'Positive feedback'];
+  const values = [good, neutral, bad, total, positivePercentage];
 
-    return (
-        <div>
-            {
-                textElements.map((item, index) => (
-                    <p className={css.item}>
-                        {item}: <span
-                            key={item}
-                            style={{ color: index === 4 && positivePercentage >= 75 ? 'green' : 'black' }}
-                            >
-                            {index !== 4 ? values[index] : values[index] + '%'}
-                            </span>;
-                    </p>
-            ))
-            }
-        </div>
-    );
-}  
+  return (
+    <div>
+      {textElements.map((item, index) => (
+        <p key={item} className={css.item}>
+          {item}:{' '}
+          <span
+            style={{
+              color: index === 4 && positivePercentage >= 75 ? 'green' : 'black',
+            }}
+          >
+            {index !== 4 ? values[index] : values[index] + '%'}
+          </span>
+          ;
+        </p>
+      ))}
+    </div>
+  );
+};
 
 export { Statistics };
